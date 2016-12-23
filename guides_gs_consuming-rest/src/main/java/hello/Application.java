5d599adcc69fec02ded4d4c4264a007fb87d4200
yuryk <yuryk@DESKTOP-TKBQB7G>
2016-12-23 20:45:22 +0300

    update main class

--- guides_gs_consuming-rest/src/main/java/hello/Application.java
+++ guides_gs_consuming-rest/src/main/java/hello/Application.java
@@ -5,12 +5,18 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author yuryk
  *
  */
+@SpringBootApplication
 public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
@@ -19,10 +25,21 @@ public class Application {
      * @param args
      */
     public static void main(String[] args) {
-	RestTemplate restTemplate = new RestTemplate();
-	Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-	LOGGER.info(quote.toString());
+	SpringApplication.run(Application.class, args);
 
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+	return args -> {
+	    Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+	    LOGGER.info(quote.toString());
+	};
     }
 
 }
